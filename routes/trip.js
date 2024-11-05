b116b634ab74c9d4186c58cc345e0399ae6220a8
victor-blanchard <159810800+victor-blanchard@users.noreply.github.com>
2024-11-05 12:09:33 +0100

    modification de la route get trips

--- routes/trip.js
+++ routes/trip.js
@@ -14,11 +14,20 @@ const Trip = require("../models/trips");
 */
 
 // Route GET qui permet de recupérer les trips qui correpondent au critères de recherche ( depart, arrivée et date précise)
-router.get("/", (req, res) => {
+router.post("/", (req, res) => {
+  // Recupération de la date dans le body de la requete (la date de requete doit etre au format YYYY-MM-DD )
+  const targetDate = new Date(req.body.date);
+  // creation de la variable de début de date
+  const startOfDay = new Date(targetDate);
+  startOfDay.setHours(0, 0, 0, 0);
+  // creation de la variable de fin de date
+  const endOfDay = new Date(targetDate);
+  endOfDay.setHours(23, 59, 59, 999);
+
   Trip.find({
     departure: { $regex: new RegExp(req.body.departure, "i") },
     arrival: { $regex: new RegExp(req.body.arrival, "i") },
-    date: req.body.date,
+    date: { $gte: startOfDay, $lte: endOfDay },
   }).then((data) => {
     if (data) {
       res.json({ result: true, trips: data });
