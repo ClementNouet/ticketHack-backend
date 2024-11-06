8709669aa6ddab82fd127270616618f07c892576
victor-blanchard <159810800+victor-blanchard@users.noreply.github.com>
2024-11-06 16:13:08 +0100

    mise a jour routes signin et up

--- routes/users.js
+++ routes/users.js
@@ -1,50 +1,46 @@
-  var express = require("express");
-  var moment = require("moment");
-  var router = express.Router();
+var express = require("express");
+var moment = require("moment");
+var router = express.Router();
 
-  require("../models/connection");
-  const fetch = require("node-fetch");
-  const User = require("../models/users");
+require("../models/connection");
+const fetch = require("node-fetch");
+const User = require("../models/users");
 
+//
+router.post("/signin", (req, res) => {
+  User.find({
+    email: req.body.email,
+    password: req.body.password,
+  }).then((data) => {
+    if (data.length !== 0) {
+      res.json({ result: true, user: data });
+    } else {
+      res.json({ result: false, error: "No User found" });
+    }
+  });
+});
 
-  //
-  router.post('/signin', (req, res) => {
-    User.find({
-      email: { $regex: new RegExp(req.body.email, "i") },
-      password: { $regex: new RegExp(req.body.password, "i") },
-    }).then((data) => {
-      if (data.length !== 0) {
-        res.json({ result: true, user: data });
+router.post("/signup", (req, res) => {
+  User.find({
+    email: req.body.email,
+  }).then(() => {
+    // Vérifier si l'utilisateur existe déjà dans la base de données
+    User.findOne({ email: req.body.email }).then((email) => {
+      if (email) {
+        res.json({ result: false, error: "User already exists" });
       } else {
-        res.json({ result: false, error: "No User found" });
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        });
+
+        newUser.save().then((newUser) => {
+          res.json({ result: true, user: newUser });
+        });
       }
     });
-  })
-
-
-  router.post('/signup', (req, res) => {
-    User.find({
-      email: { $regex: new RegExp(req.body.email, "i") },
-      password: { $regex: new RegExp(req.body.password, "i") },
-    }).then(() => {
-        // Vérifier si l'utilisateur existe déjà dans la base de données
-        User.findOne({ email: req.body.email }).then(email => {
-            if (email) {
-                res.json({ result: false, error: 'User already exists' });
-            } else {
-                    const newUser = new User({
-                        name: req.body.name,
-                        email: req.body.email,
-                        password: req.body.password
-                    });
-
-                    newUser.save().then(newUser => {
-                        res.json({ result: true, user: newUser });
-                    })
-            }
-        })
-    })
-
   });
+});
 
-  module.exports = router;
+module.exports = router;
